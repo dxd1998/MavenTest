a92c1cca6a217a72e018ffb588160774b8405fd3
dingxiaodong <992665439@qq.com>
2020-07-23 11:53:16 +0800

    主分支提交

---
+++ com.dxd.dao/src/main/java/com/dao/DepartmentMapper.java
@@ -0,0 +1,38 @@
+package com.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.pojo.Department;
+
+
+/**
+ * 部门接口
+ * @author 99266
+ *
+ */
+public interface DepartmentMapper {
+	/**
+	 * 得到所有部门
+	 * @return
+	 */
+	public List<Department> getAllDepartment();
+	/**
+	 * 删除员工部门信息
+	 * @param uId
+	 * @return
+	 */
+	public int delUserDepartment(Integer uId);
+	/**
+	 * 修改员工部门
+	 * @param map
+	 * @return
+	 */
+	public int updateUserDepartment(Map<String,Object> map);
+	/**
+	 * 添加用户部门信息
+	 * @param map
+	 * @return
+	 */
+	public int addUserDepartment(Map<String,Object> map);
+}
---
+++ com.dxd.dao/src/main/java/com/dao/UserMapper.java
@@ -0,0 +1,44 @@
+package com.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.pojo.User;
+
+/**
+ * 员工接口
+ * @author 99266
+ *
+ */
+public interface UserMapper {
+	/**
+	 * 查询所有用户信息
+	 * @return
+	 */
+	public List<User> getAllUser(Map<String,Object> map);
+	/**
+	 * 删除员工信息
+	 * @param uId
+	 * @return
+	 */
+	public int delUser(Integer uId);
+	/**
+	 * 查询指定员工
+	 * @param uId
+	 * @return
+	 */
+	public User getUserById(Integer uId);
+	/**
+	 * 修改用户信息
+	 * @param user
+	 * @return
+	 */
+	public int updateUser(User user);
+	/**
+	 * 添加员工信息
+	 * @param user
+	 * @return
+	 */
+	public int addUser(User user);
+	
+}
